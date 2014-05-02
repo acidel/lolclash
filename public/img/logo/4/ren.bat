498697b68999e4ed2f2ff68a87c89cf8a884ce6e
acidel <dylan25@gmai.com>
2014-05-01 20:53:37 -0400

    wide images

--- public/img/logo/4/ren.bat
+++ public/img/logo/4/ren.bat
@@ -4,7 +4,7 @@ Setlocal enabledelayedexpansion
 Set "Pattern= "
 Set "Replace=-"
 
-For %%# in ("C:\Users\Pius\Desktop\lologo\New folder\240\*.png") Do (
+For %%# in ("C:\Users\Pius\Desktop\lologo\New folder\240r\*.png") Do (
     Set "File=%%~nx#"
     Ren "%%#" "!File:%Pattern%=%Replace%!"
 )
