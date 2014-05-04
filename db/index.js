82ecd4fd7aa57a967fb95818693ad477bbb5027f
acidel <dylan25@gmai.com>
2014-05-04 23:22:01 +0000

    db

--- db/index.js
+++ db/index.js
@@ -12,8 +12,8 @@ var db,
 
 // Setup db connection and all that gay shit
 exports.setup = function (callback) {
-    //mongoose.connect(process.env.MONGOHQ_URL);
-    mongoose.connect('mongodb://localhost/asdf');
+    mongoose.connect(process.env.MONGOHQ_URL);
+    //mongoose.connect('mongodb://localhost/asdf');
     db = mongoose.connection;
 
     db.on('error', console.error.bind(console, 'connection error:'));
