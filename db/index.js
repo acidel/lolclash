130bca3d211a56e2a1edd15437a39def493ec56b
acidel <dylan25@gmai.com>
2014-05-06 08:04:09 +0000

    urldb

--- db/index.js
+++ db/index.js
@@ -12,7 +12,7 @@ var db,
 
 // Setup db connection and all that gay shit
 exports.setup = function (callback) {
-    mongoose.connect(process.env.MONGOHQ_URL);
+    mongoose.connect(process.env.MONGOLAB_URI);
     //mongoose.connect('mongodb://localhost/asdf');
     db = mongoose.connection;
 
